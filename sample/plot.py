51c1535717ed09e9fe07bdf3baaf98d457ee1a51
Ebihara Yusuke <yusuke@ebihara.me>
2025-03-06 19:11:23 +0900

    feat: create package

---
+++ sample/plot.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+import mpl_someyalab
+# plt.style.use("./mpl_someyalab/presentation.mplstyle")
+plt.style.use("mpl_someyalab.presentation")
+
+x = [1, 2, 3, 4, 5]
+y = [1, 4, 9, 16, 25]
+
+plt.plot(x, y, label='1')
+plt.plot(x, [2 * yi for yi in y], label='2')
+plt.plot(x, [3 * yi for yi in y], label='3')
+plt.plot(x, [4 * yi for yi in y], label='4')
+plt.plot(x, [5 * yi for yi in y], label='5')
+
+plt.xlabel("X values")
+plt.ylabel("Y values")
+plt.legend()
+plt.tight_layout()
+
+plt.show()
